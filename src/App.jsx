a5b6ee36062570571f5111f5f8fdd4a3c95d9aa1
hansoo280102 <tantran012080@gmail.com>
2024-09-30 22:42:25 +0700

    3431fe

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,13 @@ import Dashboard from "./pages/Dashboard";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Projects from "./pages/Projects";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
@@ -19,6 +22,7 @@ export default function App() {
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+/* eslint-disable no-unused-vars */
+import { Navbar } from "flowbite-react";
+import React from "react";
+
+export default function Header() {
+  return <Navbar>Header</Navbar>;
+}
