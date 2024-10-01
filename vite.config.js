f5e8518d802ade85ca514e5adf4d54748fa96009
hansoo280102 <tantran012080@gmail.com>
2024-10-01 21:14:50 +0700

    add functionality ti the sign up page

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,14 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        secure: false,
+      },
+    },
+  },
+  // Thêm dấu phẩy ở đây để phân tách server và plugins
   plugins: [react()],
 });
