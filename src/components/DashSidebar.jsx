bcc7e4ef533df3a78ee1b79c968b7b318ec3a1d6
hansoo280102 <tantran012080@gmail.com>
2024-10-11 21:12:50 +0700

    add post section to the dashboard

--- src/components/DashSidebar.jsx
+++ src/components/DashSidebar.jsx
@@ -1,14 +1,15 @@
 import { Sidebar } from "flowbite-react";
 import { useEffect, useState } from "react";
-import { HiArrowSmRight, HiUser } from "react-icons/hi";
+import { HiArrowSmRight, HiDocumentText, HiUser } from "react-icons/hi";
 import { Link, useLocation } from "react-router-dom";
 import { signOutSuccess } from "../redux/user/userSlice";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function DashSidebar() {
   const location = useLocation();
   const dispatch = useDispatch();
   const [tab, setTab] = useState("");
+  const currentUser = useSelector((state) => state.user.currentUser);
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
     const tabFromUrl = urlParams.get("tab");
@@ -32,21 +33,51 @@ export default function DashSidebar() {
       console.log(error.message);
     }
   };
+
+  // Hàm viết hoa chữ cái đầu tiên
+  const capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
   return (
     <Sidebar className="w-full md:w-56">
       <Sidebar.Items>
-        <Sidebar.ItemGroup>
+        <Sidebar.ItemGroup className="flex flex-col gap-1">
           <Link to="/dashboard?tab=profile">
             <Sidebar.Item
               active={tab === "profile"}
               icon={HiUser}
-              label={"User"}
+              label={capitalizeFirstLetter(currentUser.role)}
               labelColor="dark"
               as="div"
             >
               Profile
             </Sidebar.Item>
           </Link>
+
+          {currentUser.role === "admin" && (
+            <Link to="/dashboard?tab=posts">
+              <Sidebar.Item
+                active={tab === "posts"}
+                icon={HiDocumentText}
+                as="div"
+              >
+                All Posts
+              </Sidebar.Item>
+            </Link>
+          )}
+
+          {currentUser.role === "user" && (
+            <Link to="/dashboard?tab=posts">
+              <Sidebar.Item
+                active={tab === "posts"}
+                icon={HiDocumentText}
+                as="div"
+              >
+                My Posts
+              </Sidebar.Item>
+            </Link>
+          )}
+
           <Sidebar.Item
             icon={HiArrowSmRight}
             className="cursor-pointer"
--- src/components/OnlyAdminPrivateRoute.jsx
+++ src/components/OnlyAdminPrivateRoute.jsx
@@ -1,8 +1,12 @@
-/* eslint-disable no-unused-vars */
-import { useSelector } from "react-redux";
-import { Outlet, Navigate } from "react-router-dom";
+// /* eslint-disable no-unused-vars */
+// import { useSelector } from "react-redux";
+// import { Outlet, Navigate } from "react-router-dom";
 
-export default function OnlyAdminPrivateRoute() {
-  //   const { currentUser } = useSelector((state) => state.user);
-  //   return currentUser && currentUser.role === "admin" ? <Outlet /> : <Navigate to={"sign-in"} />;
-}
+// export default function OnlyAdminPrivateRoute() {
+//   const { currentUser } = useSelector((state) => state.user);
+//   return currentUser && currentUser.role === "admin" ? (
+//     <Outlet />
+//   ) : (
+//     <Navigate to={"sign-in"} />
+//   );
+// }
