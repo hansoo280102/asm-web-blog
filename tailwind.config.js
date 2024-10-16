df11ceaa9d26210b2b319f7800e520864bfa83c3
hansoo280102 <tantran012080@gmail.com>
2024-10-16 21:56:26 +0700

    add recent article section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@
 
 import flowbite from "flowbite-react/tailwind";
 import tailwindScrollbar from "tailwind-scrollbar";
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
   theme: {
