781cc63a9689e964296972c7d738e9caf3f43646
hansoo280102 <tantran012080@gmail.com>
2024-10-28 02:02:54 +0700

    bookmark done

--- src/components/DashBookmark.jsx
+++ src/components/DashBookmark.jsx
@@ -9,7 +9,6 @@ export default function DashBookmark() {
   const [bookmarkedPosts, setBookmarkedPosts] = useState([]);
   const [showMore, setShowMore] = useState(true);
   const [showModal, setShowModal] = useState(false);
-  const [postIdToDelete, setPostIdToDelete] = useState("");
 
   useEffect(() => {
     const fetchBookmarkedPosts = async () => {
@@ -46,28 +45,6 @@ export default function DashBookmark() {
         if (data.length < 9) {
           setShowMore(false);
         }
-      }
-    } catch (error) {
-      console.log(error.message);
-    }
-  };
-
-  const handleDeleteBookmark = async () => {
-    setShowModal(false);
-    try {
-      const res = await fetch(
-        `/api/post/removebookmark/${postIdToDelete}/${currentUser._id}`,
-        {
-          method: "DELETE",
-        }
-      );
-      const data = await res.json();
-      if (!res.ok) {
-        console.log(data.message);
-      } else {
-        setBookmarkedPosts((prev) =>
-          prev.filter((post) => post._id !== postIdToDelete)
-        );
       }
     } catch (error) {
       console.log(error.message);
@@ -128,30 +105,6 @@ export default function DashBookmark() {
       ) : (
         <p>You have no bookmarked posts yet.</p>
       )}
-      <Modal
-        show={showModal}
-        onClose={() => setShowModal(false)}
-        popup
-        size="md"
-      >
-        <Modal.Header />
-        <Modal.Body>
-          <div className="text-center">
-            <HiOutlineExclamationCircle className="h-14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto" />
-            <h3 className="mb-5 text-lg text-gray-500 dark:text-gray-400">
-              Are you sure you want to remove this bookmark?
-            </h3>
-            <div className="flex justify-center gap-4">
-              <Button color="failure" onClick={handleDeleteBookmark}>
-                Yes, I&apos;m sure
-              </Button>
-              <Button color="gray" onClick={() => setShowModal(false)}>
-                No, cancel
-              </Button>
-            </div>
-          </div>
-        </Modal.Body>
-      </Modal>
     </div>
   );
 }
